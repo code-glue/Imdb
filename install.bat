b8735fe39c5f0ae18a5895c5fa5c3367eb7c032a
skataben <skataben@gmail.com>
2014-04-27 08:20:14 -0500

    Added comments to install script and made it more portable

--- install.bat
+++ install.bat
@@ -1,17 +1,24 @@
 @echo off
-set Here=%~dp0
 
+:: This script simply copies the necessary files to the module's install location under
+:: the user's "\My Documents\WindowsPowerShell\Modules\" directory.
+:: It is not required to run the module and is included only as a convenience.
+
+set ModuleName=CodeGlue.Imdb
+
+:: Get the My Documents path
 for /f "tokens=3 delims= " %%a in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Personal"') do (set MyDocs=%%a)
-if %ErrorLevel% neq 0   echo(Failed to retrieve 'My Documents' path. && exit /b %ErrorLevel%
-if not exist "%MyDocs%" echo(Failed to retrieve 'My Documents' path. && exit /b 1
+if %ErrorLevel% neq 0 echo(Failed to retrieve 'My Documents' path. && exit /b %ErrorLevel%
 
-set moduleDir=%MyDocs%\WindowsPowerShell\Modules\CodeGlue.Imdb\
-
+:: Create the module directory
 SetLocal EnableExtensions
-if not exist "%moduleDir%" mkdir "%moduleDir%"
+set InstallDir=%MyDocs%\WindowsPowerShell\Modules\%ModuleName%\
+if not exist "%InstallDir%" mkdir "%InstallDir%"
 
-copy "%Here%CodeGlue.Imdb.*" "%moduleDir%" /y > nul
-copy "%Here%ReadMe.md"       "%moduleDir%" /y > nul
-copy "%Here%License.txt"     "%moduleDir%" /y > nul
+:: Copy all the files except this script to the module directory
+attrib "%~f0" -a
+xcopy "%~dp0*" "%InstallDir%" /e /q /i /y /a > nul
+attrib "%~f0" +a
 
-explorer "%moduleDir%"
+:: Open the file explorer to the module directory
+explorer "%InstallDir%"
